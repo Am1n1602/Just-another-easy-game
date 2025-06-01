930a2ece5e102015f156696a59121cc0e90b3e8f
Am1n1602 <amangautam1602@gmail.com>
2025-06-01 16:30:23 +0530

    Improve Map rendering logic

--- tilemap.cpp
+++ tilemap.cpp
@@ -1,5 +1,5 @@
 #include "tilemap.h"
-
+#include <iostream>
 // TODO <-Need to draw the map, rendering and drawing is implement ->
 
 static Texture gameMap = { 0 };
@@ -19,37 +19,47 @@ void TileMap::LoadMap()
 void TileMap::MakeMap()
 
 {
-	for (int i = 0;i < 20;i++) {
-		this->MapCoord += "000000000000000000000000000000000000000";
+	this->MapCoord += "01";
+	for (int i = 0; i < 38;i++) {
+		this->MapCoord += "02";
 	}
-	this->MapCoord += "000000001222222222222222222222222222223";
+	this->MapCoord += "03";
 }
 
 void TileMap::DrawMap()
 
 {
 	int size = this->MapCoord.size();
-	
+
+	int whichTile1 = -1;
+	int whichTile2 = -1;
 	int whichTile = -1;
 	int tileX = -1;
 	int tileY = -1;
 	int desX = -1;
 	int desY = -1;
 
-	for (int i = 0;i < size;i++) {
-		if (this->MapCoord[i] == '0')
-		{
+	for (int i = 0; i < (size)-1; i+=2)
+	{
+		if (this->MapCoord[i] == '0' && this->MapCoord[i + 1] == '0') {
 			continue;
 		}
 		else {
-			whichTile=this->MapCoord[i] - 1;
+			whichTile1 = this->MapCoord[i] - '0';
+			whichTile2 = this->MapCoord[i+1] - '0';
+
+			whichTile = ((10 * whichTile1) + whichTile2)-1;
+
+
 			tileX = whichTile % 8;
 			tileY = whichTile / 8;
-			desX = i % 40;
-			desY = i / 40;
+
+			desX = (i / 2) % 40;
+			desY = (i / 2) / 40;
 
 
-			DrawTexturePro(gameMap, { (float)tileX,(float)tileY,32,32 }, { (float)desX*32,(float)desY*32,32,32 }, { 0,0 }, 0.0f, WHITE);
+			DrawTexturePro(gameMap, { (float)(tileX)*32,(float)(tileY)*32,32,32 }, { (float)desX * 32,(float)desY * 32,32,32 }, { 0,0 }, 0.0f, WHITE);
+
 		}
 	}
 }
